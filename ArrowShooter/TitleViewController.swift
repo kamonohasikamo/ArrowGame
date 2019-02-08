82865b275fef373eb28c7d43b4f30f91d769a2e7
kame211031 <arrowsuke@yahoo.co.jp>
2019-02-08 18:40:41 +0900

    First Commit

---
+++ ArrowShooter/TitleViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TitleViewController.swift
+//  ArrowShooter
+//
+//  Created by suke on 2018/04/09.
+//  Copyright © 2018年 suke. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //スタートボタン
+
+    @IBAction func startButton(_ sender: AnyObject) {
+    }
+    //twitterButton
+    @IBAction func twitter(_ sender: AnyObject) {
+    }
+    //rankingButton
+    @IBAction func rankingButton(_ sender: AnyObject) {
+    }
+}
